d8acf99f98c4c4e99c37508192d329ea6be29f5c
Florian Kempenich <Flori@nKempenich.com>
2018-11-02 10:01:27 +0000

    Move MockApi and use mock 'scenarios' in testsˆ

--- tests/kata/github/test_repo.py
+++ tests/kata/github/test_repo.py
@@ -1,29 +1,67 @@
-import functools
 from pprint import pprint
 
 import pytest
 
-from src.kata.github.api import MockApi, Api
+from src.kata.github.api import Api
 from src.kata.github.repo import Repo
+
+NOT_USED = 'Not Used'
 
 
 @pytest.fixture
 def mock_api():
+    class MockApi:
+        mocked_responses = {
+            'contents': {
+                'only_files': {
+                    '': [{
+                        'name': 'a_file.txt',
+                        'path': 'a_file.txt',
+                        'type': 'file',
+                        'download_url': 'https://github_url_for/a_file.txt'
+                    }, {
+                        'name': 'another_file.py',
+                        'path': 'another_file.py',
+                        'type': 'file',
+                        'download_url': 'https://github_url_for/another_file.py'
+                    }, {
+                        'name': 'a_third_file.md',
+                        'path': 'a_third_file.md',
+                        'type': 'file',
+                        'download_url': 'https://github_url_for/a_third_file.md'
+                    }]
+                },
+                'dir_containing_files': [
+
+                ],
+                'empty_dir': [
+
+                ],
+                'nested_dirs': [
+
+                ],
+                'mixed_files_and_dir': [
+
+                ]
+
+            }
+        }
+
+        def contents(self, _user, repo, path):
+            scenario = repo
+            if scenario not in self.mocked_responses['contents']:
+                raise ValueError(f"Scenario: '{scenario}' doesn't correspond to a valid scenario")
+            if path not in self.mocked_responses['contents'][scenario]:
+                raise ValueError(f"Path: '{path}' doesn't correspond to a valid path for the scenario: '{scenario}'")
+
+            return self.mocked_responses['contents'][scenario][path]
+
     return MockApi()
 
 
 @pytest.fixture
 def repo(mock_api):
     return Repo(mock_api)
-
-
-@pytest.fixture
-def file_urls_for_given_path(repo):
-    @functools.wraps(repo.file_urls)
-    def wrapper(path):
-        return repo.file_urls(user='NotUsed', repo='NotUsed', path=path)
-
-    return wrapper
 
 
 @pytest.mark.skip
@@ -33,18 +71,23 @@ def test_sandbox():
     pprint(res)
 
 
-def test_only_files(file_urls_for_given_path):
-    assert file_urls_for_given_path('only_files') == [
+def test_only_files(repo):
+    assert repo.file_urls(NOT_USED, repo='only_files', path='') == [
         {
-            'file_path': '.travis.yml',
-            'download_url': 'https://github_url_for/.travis.yml'
+            'file_path': 'a_file.txt',
+            'download_url': 'https://github_url_for/a_file.txt'
         },
         {
-            'file_path': 'LICENSE.md',
-            'download_url': 'https://github_url_for/LICENSE.md'
+            'file_path': 'another_file.py',
+            'download_url': 'https://github_url_for/another_file.py'
         },
         {
-            'file_path': 'README.md',
-            'download_url': 'https://github_url_for/README.md'
+            'file_path': 'a_third_file.md',
+            'download_url': 'https://github_url_for/a_third_file.md'
         }
     ]
+
+
+@pytest.mark.skip
+def test_directory_containing_files(repo):
+    pass
